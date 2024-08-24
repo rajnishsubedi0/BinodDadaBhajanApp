8f5f02e995486e83ae7e708d4b1790edad0ce97c
Rajnish Subedi <rajnishsubedi0@gmail.com>
2024-08-24 16:31:28 +0545

    added image loader

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.android.volley:volley:1.2.1")
     implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 
 
 }
--- app/src/main/java/com/example/realbhajanbinoddada/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/realbhajanbinoddada/ui/gallery/GalleryFragment.java
@@ -1,31 +1,39 @@
 package com.example.realbhajanbinoddada.ui.gallery;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.example.realbhajanbinoddada.R;
 import com.example.realbhajanbinoddada.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
+        public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        String url="https://wallpaperaccess.com/full/265770.jpg";
+        ImageView imageView=root.findViewById(R.id.my_image);
+        Glide.with(getContext()).load(url)
+                .centerCrop()
+                .into(imageView);
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -34,4 +42,6 @@ public class GalleryFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
